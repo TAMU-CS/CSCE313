58aff2944f4fc7e40f4a079fdf9498b4632c7e47
tingtongtang <kdt0116@tamu.edu>
2019-02-17 18:49:44 -0600

    testing

--- PA2/client.cpp
+++ PA2/client.cpp
@@ -22,20 +22,15 @@ int main(int argc, char *argv[]){
     int *len = new int(100);
     char *buffer = new char(100);
 
-    double curTime = 0;
     for(int i = 0; i < 15000; i++){ //15000 different times
         //send a request for information (person, seconds, ecg number)
-        cout << "TESTing" << endl;
         datamsg *request = new datamsg(DATA_MSG, 60, 1);
 
         chan.cwrite((char*)request, 100);
         cout << "TESTing" << endl;
 
         buffer = chan.cread(len);
-        cout << "TESTing" << endl;
-
-        curTime += 0.04;
-
+        cout << buffer << endl;
     }
     delete len;
 
