c53d5e2a892ba0c79d674f3b20397cedc5048798
Kevin <kdt0116@tamu.edu>
2019-02-19 15:33:07 -0600

    Finished part 3 and 4, started part 5 running server as child process

--- PA2/client.cpp
+++ PA2/client.cpp
@@ -42,7 +42,7 @@ int main(int argc, char *argv[]){
     person1output.close();
     */
 
-    //2. request a test File
+    /*2. request a test File
     //get the length
     char * fileName = "1.csv";
     filemsg *fmsg = new filemsg(0, 0);
@@ -81,6 +81,95 @@ int main(int argc, char *argv[]){
     buffer = chan.cread(resultLength);
     y1output << buffer;
     y1output.close();
+    */
+
+   	/*3. request binary file
+
+    char * fileName = "mybin";
+    filemsg *fmsg = new filemsg(0, 0);
+	int requestSize = sizeof(filemsg) + sizeof(char) * 5 + 1;
+    
+    char * request = new char[requestSize];
+    *((filemsg *) request) = *fmsg;
+
+    strcpy(request + sizeof(filemsg), fileName);
+    chan.cwrite(request, requestSize);
+
+    int * resultLength = new int(0);
+    int size = *((int*)chan.cread(resultLength));
+	cout << size << endl;
+    //send a request for information (person, seconds, ecg number)
+    ((filemsg *) request)->length = 100;
+    ((filemsg *) request)->offset = 0;
+
+    //file setup
+	FILE* y1output = fopen ("BinCpyOutput", "wb");
+
+    //loop and request
+    char *buffer;
+    for(int i = 100; i < size; i+= 100){
+        ((filemsg *) request)->offset = i - 100;
+
+        chan.cwrite(request, requestSize);
+        buffer = chan.cread(resultLength);
+
+		fseek (y1output, ((filemsg *) request)->offset, SEEK_SET);
+		fwrite (buffer, 1, 100, y1output);
+    }
+
+	if(size % 100 != 0){
+
+		//set the request length to the ending
+		((filemsg *) request)->length = size % 100;
+		((filemsg *) request)->offset = size  - (size % 100);
+		
+		chan.cwrite(request, requestSize);
+
+		buffer = chan.cread(resultLength);
+		fseek (y1output, ((filemsg *) request)->offset, SEEK_SET);
+		fwrite (buffer, 1, size % 100, y1output);
+	}
+ 
+    fclose(y1output);
+	*/
+
+	/*4. Requesting a new channel
+	datamsg *request = new datamsg(1, 0, 1);
+	request->mtype = NEWCHANNEL_MSG;
+	chan.cwrite((char *) request, sizeof(datamsg));
+
+	int *size = new int();
+	char *requestedChannel = chan.cread(size);
+	cout << requestedChannel << endl;
+    FIFORequestChannel customChannel (requestedChannel, FIFORequestChannel::CLIENT_SIDE);
+
+    ofstream person1output("customRequest.csv");
+    int *len = new int(100);
+    char *buffer = new char(100);
+
+    for(int i = 0; i < 100; i++){ //15000 different times
+        //send a request for information (person, seconds, ecg number)
+        *request = datamsg(1, (double)i * 0.004, 2);
+        person1output << (double)i * 0.004 << ",";
+
+        chan.cwrite((char*)request, 100);
+        buffer = chan.cread(len);
+        person1output << *((double*)buffer) << ",";
+
+        *request = datamsg(1, (double)i * 0.004, 2);
+        chan.cwrite((char*)request, 100);
+        buffer = chan.cread(len);
+        person1output << *((double*)buffer) << endl;
+
+    }
+    delete len;
+    person1output.close();
+	*/
+
+	/*5.Run server as child process and then send quit msg
+	*/
+
+	sleep(10); //sleep for 10 seconds
 
 	return 0;
 }
